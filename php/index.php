e7685b3b2c2e1ca9d4d0679e568b66c39731b127
avareno <miguelcabral798@gmail.com>
2024-04-17 19:12:02 +0100

    problems in css/php

--- php/index.php
+++ php/index.php
@@ -1,20 +1,23 @@
+<?php
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
     <title>LTW</title>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/topnavbarstyle.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/topnavbarstyle.css?v=<?php echo time(); ?>">
 
-    
-    <link rel="stylesheet" href="../css/navbarstyle.css">
   </head>
   <header>
     <ul class="topnav">
       <li><img src="https://upload.wikimedia.org/wikipedia/commons/1/1f/The_IMG_Media_broadcasting_company_logo.png"></li>
       <li><a class="active" href="#home">Home</a></li>
-      <li class = "right"><input type="text" placeholder="Search.."></li>
-      <li><a  href="#news">News</a></li>
+      <li class="right"><input type="text" placeholder="Search.."></li>
+      <li><a href="#news">News</a></li>
       <li><a href="#contact">Contact</a></li>
     </ul>
 
