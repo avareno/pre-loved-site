2f46692b85ecf1166a446d9b813ca189e86a144c
avareno <miguelcabral798@gmail.com>
2024-04-18 08:10:40 +0100

    Moved css to php

--- php/index.php
+++ php/index.php
@@ -1,4 +1,7 @@
 <?php
+
+
+
 ?>
 
 <!doctype html>
@@ -6,18 +9,18 @@
   <head>
     <title>LTW</title>
     <!-- Required meta tags -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../css/topnavbarstyle.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/topnavbarstyle.css?v=<?php echo time(); ?>">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    
+    <link rel="stylesheet" href="../style.css">
   </head>
   <header>
     <ul class="topnav">
       <li><img src="https://upload.wikimedia.org/wikipedia/commons/1/1f/The_IMG_Media_broadcasting_company_logo.png"></li>
       <li><a class="active" href="#home">Home</a></li>
-      <li class="right"><input type="text" placeholder="Search.."></li>
-      <li><a href="#news">News</a></li>
+      <li class = "right"><input type="text" placeholder="Search.."></li>
+      <li><a  href="#news">News</a></li>
       <li><a href="#contact">Contact</a></li>
     </ul>
 
