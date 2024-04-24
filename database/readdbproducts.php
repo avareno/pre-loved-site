00575209737de2de7fe5956261dc6ce048e112f2
avareno <miguelcabral798@gmail.com>
2024-04-24 10:33:47 +0100

    carousel implementation

--- database/readdbproducts.php
+++ database/readdbproducts.php
@@ -4,4 +4,8 @@
     $stmt = $db->prepare('Select * from products ');
     $stmt->execute();
     $products = $stmt->fetchAll();
+
+    $stmt = $db->prepare('Select * from images ');
+    $stmt->execute();
+    $images = $stmt->fetchAll();
 ?>
