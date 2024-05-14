cdb639f0412b32cac56827b2a4184b3137bcdd01
Luís Cordeiro <kammeratlego14@gmail.com>
2024-05-14 20:59:47 +0100

    Refeito o carrinho

---
+++ templates/shopping_cart_tpl.php
@@ -0,0 +1,35 @@
+<?php
+function drawShoppingCart($db, $products){
+?>
+<!doctype html>
+<html lang="en">
+
+    <section class="grid-container">
+        <h1>Carrinho de Compras</h1>
+        <?php if (count($products) > 0) : ?>
+            <ul>
+            <?php foreach ($products as $product) : ?>
+        <li>
+            <img src="<?php echo $product['carousel_img']; ?>" alt="<?php echo $product['title']; ?>"> 
+            <p class="product-title"><?php echo $product['title']; ?></p>
+            <p class="product-description"><?php echo $product['description']; ?></p> 
+            <form action="../cart/remove_from_cart.php" method="post">
+                <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+                <button type="submit" name="remove_from_cart">Remover do Carrinho</button>
+            </form>
+        </li>
+    <?php endforeach; ?>
+
+            </ul>
+        <?php else : ?>
+            <p>O seu carrinho está vazio.</p>
+        <?php endif; ?>
+    </section>
+
+    
+</body>
+
+</html>
+<?php
+}
+?>
