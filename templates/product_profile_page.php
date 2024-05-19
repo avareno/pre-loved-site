d5754e3a9c224932724a1bd3a727f57086b305a0
Luís Cordeiro <kammeratlego14@gmail.com>
2024-05-19 15:24:51 +0100

    Redirecionamento para o perfil quando o produto for do vendedor

--- templates/product_profile_page.php
+++ templates/product_profile_page.php
@@ -1,9 +1,10 @@
 <?php
 function drawProductProfile($product, $productImage, $product_id)
 {
+    $current_user_id = $_SESSION['id'];
     ?>
     <link rel="stylesheet" href="../../../css/product_profile.css">
-
+    
 
     <section class="grid-container">
         <section class="product-images">
@@ -21,23 +22,25 @@ function drawProductProfile($product, $productImage, $product_id)
                     <p class="right"><?php echo $product['category']; ?></p>
                     <p class="left">Seller</p>
                     <p class="right">
-                        <a href="../profile/public_profile.php?id=<?php echo $product['seller_id']; ?>">
-                        <?php echo $product['seller_id']; ?>
+                    <?php if ($product['seller_id'] == $current_user_id): ?>
+                        <a href="../profile/profile.php"><?php echo htmlspecialchars($product['seller_id']); ?></a>
+                    <?php else: ?>
+                        <a href="../profile/public_profile.php?id=<?php echo htmlspecialchars($product['seller_id']); ?>">
+                            <?php echo htmlspecialchars($product['seller_id']); ?>
                         </a>
-                    </p>
+                    <?php endif; ?>
+                </p>
 
                 </section>
             </section>
             <h1><?php echo $product['title']; ?></h1>
-            <!-- Verifica se a quantidade do produto é maior que zero -->
+        
             <?php if ($product['quantity'] > 0): ?>
-                <!-- Se a quantidade for maior que zero, exibe o botão de adicionar ao carrinho -->
                 <form action="../../actions/add_to_cart.php" method="post">
                     <input type="hidden" name="product_id" value="<?php echo $product_id; ?>">
                     <input type="submit" value="Add to Cart">
                 </form>
             <?php else: ?>
-                <!-- Se a quantidade for zero, desativa o botão de adicionar ao carrinho e exibe a mensagem -->
                 <section id="out-of-stock-container">
                     <button disabled id="out-of-stock-button">Add to Cart</button>
                     <p id="out-of-stock-message">Produto Indisponivel</p>
