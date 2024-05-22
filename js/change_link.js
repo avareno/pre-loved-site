ed0b2fbc308574922e0c7d7d6f7a65aacadbbcf4
Luís Cordeiro <kammeratlego14@gmail.com>
2024-05-22 10:18:58 +0100

    fixed js

--- js/change_link.js
+++ js/change_link.js
@@ -1,7 +1,7 @@
 function showSection(sectionId) {
   // Hide all sections
-  const sections = document.getElementsByClassName("section");
-  for (const i = 0; i < sections.length; i++) {
+  let sections = document.getElementsByClassName("section");
+  for (let i = 0; i < sections.length; i++) {
     sections[i].classList.remove("active");
   }
 
